d75bcbaa852d3a1327b0b91c8e0baf57055e4881
Akshay <akshaygoud1015@gmail.com>
2024-02-12 17:02:31 +0530

    stuck at collecting data

--- Readme.txt
+++ Readme.txt
@@ -3,6 +3,7 @@
 1. npm init ( change entry point to main.js)
 2. npm install --save-dev electron
 3. npm install bootstrap@5.3.2
+4. npm install sqlite3
 
 
 
--- main.js
+++ main.js
@@ -1,31 +1,57 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
-const { app, BrowserWindow } = require('electron');
+// Connect to SQLite database
+const db = new sqlite3.Database('client.db');
 
+// Create table to store client details
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS clients (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        mobile TEXT,
+        gender TEXT,
+        address TEXT,
+        age INTEGER,
+        fee REAL,
+        payment_duration TEXT
+    )`);
+});
 
+// Listen for insert-client event from renderer process
+ipcMain.on('insert-client', (event, clientData) => {
+    // Insert data into SQLite database
+    db.run(`INSERT INTO clients (name, mobile, gender, address, age, fee, payment_duration)
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            Object.values(clientData),
+            (err) => {
+                if (err) {
+                    event.reply('insert-client-reply', err.message);
+                } else {
+                    event.reply('insert-client-reply', null);
+                }
+            });
+});
 
 function createWindow() {
-    
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            preload: path.join(__dirname,'src', 'preload.js')
         }
     });
 
-    
-    mainWindow.loadFile('index.html');
-
+    mainWindow.loadFile(path.join(__dirname, 'src', 'index.html'));
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed, except on macOS. There,
-// it's common for applications and their menu bar to stay
-// active until the user quits explicitly with Cmd + Q.
+// Quit when all windows are closed, except on macOS.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
