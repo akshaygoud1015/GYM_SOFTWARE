a494d11bd7610d800a6f102e020fae0cdbef515d
Akshay <akshaygoud1015@gmail.com>
2024-02-13 16:26:36 +0530

    shifted to mysql

--- main.js
+++ main.js
@@ -1,38 +1,23 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const mysql = require('mysql2');
 
-// Connect to SQLite database
-const db = new sqlite3.Database('client.db');
 
-// Create table to store client details
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS clients (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        mobile TEXT,
-        gender TEXT,
-        address TEXT,
-        age INTEGER,
-        fee REAL,
-        payment_duration TEXT
-    )`);
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin',
+    database: 'gymclient'
 });
 
-// Listen for insert-client event from renderer process
-ipcMain.on('insert-client', (event, clientData) => {
-    // Insert data into SQLite database
-    db.run(`INSERT INTO clients (name, mobile, gender, address, age, fee, payment_duration)
-            VALUES (?, ?, ?, ?, ?, ?, ?)`,
-            Object.values(clientData),
-            (err) => {
-                if (err) {
-                    event.reply('insert-client-reply', err.message);
-                } else {
-                    event.reply('insert-client-reply', null);
-                }
-            });
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
 });
+
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
@@ -47,11 +32,9 @@ function createWindow() {
     mainWindow.loadFile(path.join(__dirname, 'src', 'index.html'));
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed, except on macOS.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
@@ -59,7 +42,6 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    // On macOS, re-create a window when the dock icon is clicked
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
